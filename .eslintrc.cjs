2c8428cb1a0f16f1d70ded0421c70cf48dc850ce
veritablevein <veritablevein@gmail.com>
2023-06-06 19:44:21 +0800

    chore: use vite init dev environment
    vue3.3 ts vite vue-router pinia vitest cypress eslint prettier husky lint-staged commitizen
    commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
+    // '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
