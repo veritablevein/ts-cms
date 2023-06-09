6593a648895c9c0a863596f0a3277c78d9ccaaf2
veritablevein <veritablevein@gmail.com>
2023-06-10 00:11:05 +0800

    feat: add router guards login to main after get token

--- src/stores/login/login.ts
+++ src/stores/login/login.ts
@@ -1,9 +1,10 @@
+import { LOGIN_TOKEN } from '@/global/constants'
+import router from '@/router'
 import { accountLoginRequest } from '@/services/login/login'
 import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
 import { defineStore } from 'pinia'
 
-const LOGIN_TOKEN = 'login/token'
 const useLoginStore = defineStore('login', {
   state: () => ({
     id: '',
@@ -19,6 +20,8 @@ const useLoginStore = defineStore('login', {
       this.token = loginResult.data.token
 
       localCache.setCache(LOGIN_TOKEN, this.token)
+
+      router.push('/main')
     }
   }
 })
